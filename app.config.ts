82c98a3e0c97e101a64e5fa1b87647c30c88b697
mohamadreza <mrmb94@gmmail.com>
2024-10-13 14:59:20 +0330

    init

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,10 @@
 import appProductionConfig from './app.json';
-
+import appStageConfig from "./app-stage.json"
 module.exports = () => {
     if (process.env.EXPO_PUBLIC_ENV === 'PRODUCTION') {
         return appProductionConfig;
     } else if (process.env.EXPO_PUBLIC_ENV === 'STAGE') {
-        return {};
+        return appStageConfig;
     } else {
         return {};
     }
